9c8218d92b1ea99e0beba24b9e96e84ff47c31b0
ItaloF532 <italo.francisco@startse.com>
2023-10-02 19:40:49 -0300

    style: adjsut style of login page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -5,8 +5,11 @@ import LoginForm from "../../components/LoginForm";
 const LoginPage: React.FC = () => {
   return (
     <div className="login-container">
-      <h1>Login</h1>
-      <LoginForm />
+      <h1> WebChat for AS64A Exam </h1>
+      <div className="login-form">
+        <h2>Login</h2>
+        <LoginForm />
+      </div>
     </div>
   );
 };
