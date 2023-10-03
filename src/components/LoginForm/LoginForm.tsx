ddfdac09f2ab6d7c48f7c57375bb92ab981824df
ItaloF532 <italo.francisco@startse.com>
2023-10-03 12:02:53 -0300

    refactor: adjust auth controller and login page to deal with errors and redirect user on success

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,39 +1,57 @@
+import { Navigate } from "react-router-dom";
+import AuthController from "../../infra/controllers/AuthController";
 import "./style.css";
 import React, { useState } from "react";
 
 const LoginForm: React.FC = () => {
+  const authController = new AuthController();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    // Perform login logic here, e.g., send a request to your backend.
-    console.log("Login clicked");
+  const handleLogin = async () => {
+    try {
+      await authController.signIn(username, password);
+      setIsLoggedIn(true);
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.toString() === "Error: Invalid credentials!") {
+          alert("Invalid usernamer or password!");
+          return;
+        }
+      }
+
+      throw error;
+    }
   };
 
   return (
-    <form id="post-form">
-      <div className="form-field">
-        <label>Username:</label>
-        <input
-          type="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+    <>
+      {isLoggedIn && <Navigate to="/chat" replace={true} />}
+      <form id="post-form">
+        <div className="form-field">
+          <label>Username:</label>
+          <input
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <button type="button" onClick={handleLogin}>
-        Login
-      </button>
-    </form>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/infra/controllers/AuthController.ts
+++ src/infra/controllers/AuthController.ts
@@ -1,11 +1,17 @@
+import { AxiosError } from "axios";
 import HttpService from "../services/http";
+import Cookies from "js-cookie";
 
 class AuthController {
   private http = new HttpService();
 
-  async signIn(username: string, password: string): Promise<string> {
+  private async setTokenCookie(token: string): Promise<void> {
+    Cookies.set("token", token, { expires: 1 });
+  }
+
+  async signIn(username: string, password: string): Promise<void> {
     try {
-      const res = await this.http.post({
+      const res = await this.http.post<{ token: string }>({
         path: "/login",
         body: {
           username,
@@ -13,12 +19,16 @@ class AuthController {
         },
       });
 
-      console.log(res);
+      if (res?.data?.token) this.setTokenCookie(res.data.token);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const data = err.response?.data as { message: string };
+        if (data?.message === "Invalid credentials!") {
+          throw new Error("Invalid credentials!");
+        }
+      }
 
-      return "";
-    } catch (error) {
-      console.log(error);
-      throw error;
+      throw err;
     }
   }
 }
