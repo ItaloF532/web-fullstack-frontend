c0d362204a3ae5accb802ec3fd9f8173d23d51e2
ItaloF532 <italo.francisco@startse.com>
2023-10-06 11:24:29 -0300

    style: adjust send icon color

--- src/assets/SendIcon.tsx
+++ src/assets/SendIcon.tsx
@@ -3,20 +3,16 @@ import React from "react";
 const SendIcon: React.FC = () => {
   return (
     <svg
-      enableBackground={"new 0 0 24 24;"}
-      version="1.1"
       viewBox="0 0 24 24"
       xmlSpace="preserve"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
+      enableBackground="new 0 0 24 24"
     >
-      <g id="info" />
-      <g id="icons">
-        <path
-          d="M21.5,11.1l-17.9-9C2.7,1.7,1.7,2.5,2.1,3.4l2.5,6.7L16,12L4.6,13.9l-2.5,6.7c-0.3,0.9,0.6,1.7,1.5,1.2l17.9-9   C22.2,12.5,22.2,11.5,21.5,11.1z"
-          id="send"
-        />
-      </g>
+      <path
+        d="m21.5 11.1-17.9-9c-.9-.4-1.9.4-1.5 1.3l2.5 6.7L16 12 4.6 13.9l-2.5 6.7c-.3.9.6 1.7 1.5 1.2l17.9-9c.7-.3.7-1.3 0-1.7z"
+        fill="#007bff"
+        className="fill-000000"
+      ></path>
     </svg>
   );
 };
